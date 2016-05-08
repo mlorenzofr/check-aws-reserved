f471637c06c147c2dd9eb897c86b6748ca232473
Manuel Lorenzo <manuel@trovit.com>
2016-05-08 23:25:29 +0200

    First beta

---
+++ check_aws_reserved/__main__.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+from sys import exit
+import argparse
+import boto3
+import datetime
+from dateutil.tz import tzutc, tzlocal
+
+
+states = ['ok', 'warning', 'critical', 'unknown']
+
+
+def get_reservations(profile):
+    session = boto3.Session(profile_name=profile)
+    ec2 = session.client('ec2')
+    filters = [{'Name': 'state', 'Values': ['active']}]
+    rsv_instances = ec2.describe_reserved_instances(Filters=filters)
+    return rsv_instances['ReservedInstances']
+
+
+def get_options():
+    hlpDsc = "Check the expiration time of AWS reserved instances"
+    optParser = argparse.ArgumentParser(description=hlpDsc)
+    optParser.add_argument("-c", "--critical", required=True,
+                           help="critical threshold",
+                           metavar="days", type=int,
+                           dest="crit", default=15)
+    optParser.add_argument("-p", "--profile", required=True,
+                           help="AWS profile in credentials file",
+                           metavar="profile", type=str,
+                           dest="profile", default='')
+    optParser.add_argument("-w", "--warning", required=True,
+                           help="warning threshold",
+                           metavar="days", type=int,
+                           dest="warn", default=30)
+    return optParser.parse_args()
+
+
+def check_instances(instances, warning, critical):
+    global_state = 0
+    for ri in instances:
+        state = 0
+        remaining = ri['End'] - datetime.datetime.now(tzutc())
+        if remaining.days <= warning:
+            if remaining.days <= critical:
+                state = 2
+            else:
+                state = 1
+        print('%s: %s %s instances expires on %s' %
+              (states[state][0].upper(),
+               ri['InstanceCount'],
+               ri['InstanceType'],
+               ri['End'].astimezone(tzlocal()).strftime('[%x %X]')))
+        if state > global_state:
+            global_state = state
+    return global_state
+
+
+def main():
+    state = 3
+    args = get_options()
+    if args.crit > args.warn:
+        print('Critical threshold cannot be higher than warning')
+    else:
+        try:
+            rsv_inst = get_reservations(args.profile)
+        except Exception:
+            print('Error retrieving AWS info')
+        else:
+            state = check_instances(rsv_inst, args.warn, args.crit)
+    exit(state)
+    return
+
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(name='check_aws_reserved',
+      version='0.1',
+      description='Check the expiration time of AWS reserved instances',
+      url='',
+      author='Manuel Lorenzo Frieiro',
+      author_email='mlorenzofr@gmail.com',
+      maintainer='Manuel Lorenzo Frieiro',
+      maintainer_email='mlorenzofr@gmail.com',
+      license='BSD',
+      keywords='nagios aws amazon ec2 instances',
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'Environment :: Plugins',
+          'Intended Audience :: System Administrators',
+          'License :: OSI Approved :: BSD License',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: System :: Monitoring',
+          'Topic :: Utilities'
+      ],
+      packages=['check_aws_reserved'],
+      install_requires=[
+          'boto3',
+          'python-dateutil'
+      ],
+      entry_points={
+          'console_scripts':
+              ['check_aws_reserved = check_aws_reserved.__main__:main']
+      },
+      zip_safe=False)
